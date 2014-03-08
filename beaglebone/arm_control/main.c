ae0587b56ddfa1faadb2d03b0b05a0339c38461d
damiensh22 <damien.quiguer@gmail.com>
2014-03-08 17:52:35 +0100

    test git damien

--- beaglebone/arm_control/main.c
+++ beaglebone/arm_control/main.c
@@ -37,6 +37,12 @@
 
 motor_t motors[NB_MOTORS];      /**< array with all controled motors */
 
+void test_git (){
+	   printf ("hello\n");	
+}
+
+	
+
 /** Display a received message from the client
  *
  * "Pretty" print of the message. display differs following message type.
